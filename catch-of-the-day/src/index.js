39d438c63033b8e4647559a19005a325983c140c
Kevin Turney <super996@optonline.net>
2016-12-26 22:02:28 -0500

    import css

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+// loads everything from the react library in to variable React, es6 module
+import React from 'react';
+import {render} from 'react-dom'; // importing just the render method from 'react-dom'
+import './css/style.css'; // rather than a link in our index.html,  let webpack do it
+import StorePicker from './components/StorePicker.js';
+
+render(<StorePicker/>, document.querySelector('#main'));
