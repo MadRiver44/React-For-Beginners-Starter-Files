ef5fb7b391a80bcbcb9c68dfc4b2ffcd1b935df5
Kevin Turney <super996@optonline.net>
2016-12-27 00:42:12 -0500

    react router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,25 @@
 // loads everything from the react library in to variable React, es6 module
 import React from 'react';
 import {render} from 'react-dom'; // importing just the render method from 'react-dom'
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css'; // rather than a link in our index.html,  let webpack do it
 import App from './components/App';
-
-
+import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker.js';
 
-render(<App/>, document.querySelector('#main'));
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+    <div>
+      <Match exactly pattern="/" component={StorePicker} />
+      <Match pattern="/store/:storeID" component={App} />
+      <Miss component={NotFound} />
+       </div>
+    </BrowserRouter>
+
+    )
+}
+
+render(<Root/>, document.querySelector('#main'));
