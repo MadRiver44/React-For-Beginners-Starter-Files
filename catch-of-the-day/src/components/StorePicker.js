efbbd99a324cc552f9b8363bd9529dc64bd184e7
Kevin Turney <super996@optonline.net>
2016-12-27 10:58:34 -0500

    handling events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  /*
+  one way....
+  One way to get this.storeInput to reference StorePicker class is to bind the goToStore function
+  ehen the constructor is called. Since we are extending a class via React, we use super,
+
+
+  constructor(){  // code that runs when that component is created
+    super(); // allows us t sprinkle the extra stuff onto the Component
+      this.gotToStore =  this.goToStore.bind(this);
+  }
+
+  */
+
+  goToStore(event) {
+    event.preventDefault(); // stops default from happening
+    console.log('you changed url');
+    // first, grab text from box
+     // const value = $('input').val();
+    console.log(this.storeInput.value); // this is null, this is not pointing to StorePicker!!, how to get the right this?
+                                  // we must have a reference in the constructor of StorePicker, an es6 thing
+                                  // in this scope, before we get to render
+    // second, we transition from / to /store/:storeId
+
+
+  }
+
   render() {
+    {/*onSubmit={ this.goToStore.bind(this) } } or....*/}
     return (
-      <form className="store-selector">
+      <form className="store-selector"  onSubmit={(e) => this.goToStore(e) }>
         <h2>Please Enter a Store Name</h2>
-        <input type="text" required placeholder="Sore Name"/>
+        <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={ (input) => {this.storeInput = input }} />
         <button type="Submit">Visit Store </button>
       </form>
 
